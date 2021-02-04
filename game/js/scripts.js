467c55af4dd1f8288707fd792782f00007ab7f7c
Martin Gundtoft <marg@kea.dk>
2021-02-04 17:18:11 +0100

    added game

---
+++ game/js/scripts.js
@@ -0,0 +1,8 @@
+let images = ["images/strawberry.jpg", "images/lime.jpg", "images/car.jpg",
+    "images/ball.jpg", "images/ball2.jpg", "images/cat.jpg", "images/hat.jpg", "images/flower.jpg", "images/tree.jpg", "images/sea.jpg", "images/cow.jpg", "images/food.jpg", "images/mask.jpg"];
+
+let output = document.getElementById("output");
+output.style.background.images[0];
+
+{/* <div style="background:yourimage.jpg no-repeat;height:imageheight px;width:imagewidth px">
+</div> */}
