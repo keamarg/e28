71f6e12d1c55dd4f212b32de49b9db0154e186dc
Martin Gundtoft <marg@kea.dk>
2021-02-23 12:38:28 +0100

    added shopping list functionality

--- practice/script.js
+++ practice/script.js
@@ -2,17 +2,24 @@ const Practice = {
     data() {
         return {
             item: "",
+            qty: "",
             items: [],
+            itemCount: 0,
             empty: true,
         }
     },
     methods: {
         addItem() {
-            this.items.push(this.item);
+            this.items.push([this.qty, this.item]);
+            this.itemCount = this.qty + this.itemCount;
+            this.qty = "";
+            this.item = "";
             this.empty = false;
+            qtyLabel.focus();
         },
-        removeItem() {
-            this.items.pop(this.item);
+        removeItem(event) {
+            this.itemCount = this.itemCount - (this.items[event.target.parentNode.value][0]);
+            this.items.splice(event.target.parentNode.value, 1);
             if (this.items.length < 1) {
                 this.empty = true;
             }
