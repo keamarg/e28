5fadca090992e233d91781367717adac22b721f6
Martin Gundtoft <marg@kea.dk>
2021-04-05 17:51:21 +0200

    updated ProductPage

---
+++ zipfoods/src/components/pages/ProductPage copy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="product-page">
+    <h1>product:{{ id }}</h1>
+    <h2>name:{{ name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+  },
+};
+</script>
+  
+<style>
+</style>
--- zipfoods/src/components/pages/ProductPage.vue
+++ zipfoods/src/components/pages/ProductPage.vue
@@ -1,18 +1,39 @@
 <template>
   <div id="product-page">
-    <h1>product:{{ id }}</h1>
+    <template v-for="product in products">
+      <show-product
+        v-if="product.id == id"
+        v-bind:key="product.id"
+        v-bind:product="product"
+      ></show-product>
+    </template>
+    <span v-if="id > 10">
+      <p>Product {{ id }} not found</p>
+      <a href="/products">Back to products page</a>
+    </span>
   </div>
 </template>
 
 <script>
+import ShowProduct from "@/components/ShowProduct.vue";
+import { products } from "@/common/products.js";
 export default {
   props: {
     id: {
       type: String,
     },
+    name: {
+      type: String,
+    },
+  },
+  components: { "show-product": ShowProduct },
+  data() {
+    return {
+      products: products,
+    };
   },
 };
 </script>
-
+  
 <style>
 </style>
