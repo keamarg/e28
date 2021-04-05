6e8433ef8f7d701d3265bad7bb5408c528670b9c
Martin Gundtoft <marg@kea.dk>
2021-04-05 13:09:52 +0200

    Deploying ZipFoods application

---
+++ zipfoods/src/components/pages/ProductPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="product-page">
+    <h1>product:{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
