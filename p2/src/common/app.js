401b1e05d3880c7cc1bf21e13dcb286bea8fdb03
Martin Gundtoft <marg@kea.dk>
2021-04-21 02:11:02 +0200

    p2 added production server API

--- p2/src/common/app.js
+++ p2/src/common/app.js
@@ -1,5 +1,5 @@
 export const axios = require('axios').create({
-    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28api.hesdev.loc',
+    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28api.hesdev.com',
     // baseURL: process.env.VUE_APP_API_URL ?? 'https://api.unsplash.com',
     responseType: 'json'
 })
